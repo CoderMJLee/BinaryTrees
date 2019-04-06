67bdc5f202b3105b46c5a4d51f65f5922c04b8ff
M了个J <199109106@qq.com>
2019-04-06 10:17:42 +0800

    Update

--- BinaryTreePrinter/src/com/mj/example/Main.java
+++ BinaryTreePrinter/src/com/mj/example/Main.java
@@ -9,50 +9,11 @@ import com.mj.printer.BinaryTreeInfo;
 import com.mj.printer.BinaryTrees.PrintStyle;
 
 public class Main {
-	static final String LINE = "----------------------------------";
-	static List<BinarySearchTree<Integer>> bsts = new ArrayList<>();
 	
-	static BinarySearchTree<Integer> bst(Integer ints[]) {
-		if (ints == null) return null;
-		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
-		for (Integer integer : ints) {
-			bst.add(integer);
-		}
-		return bst;
-	}
-	
-	static {
-		bsts.add(bst(new Integer[]{
-				7,4,9,2,5,8,11,1,3,6,10,12
-			}));
-		bsts.add(bst(new Integer[]{
-				381, 12, 410, 9, 40, 394, 540, 
-				35, 190, 476, 760, 146, 445,
-				600, 800
-			}));
-		
-		// 二叉树数量
-		int bstCount = 3;
-		for (int i = 0; i < bstCount; i++) {
-			BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
-			bsts.add(bst);
-			
-			// 二叉树的节点数量
-			int nodeCount = (int) (10 + Math.random() * 30);
-			for (int j = 0; j < nodeCount; j++) {
-				double random = Math.random();
-				// 每个节点的值
-				int element = 0;
-				if (random > 0.6) {
-					element = (int) (Math.random() * 10000);
-				} else if (random > 0.3) {
-					element = (int) (Math.random() * 1000);
-				} else {
-					element = (int) (Math.random() * 100);
-				}
-				bst.add(element);
-			}
-		}
+	public static void main(String[] args) {
+		tree1();
+		tree2();
+		tree3();
 	}
 	
 	static void tree1() {
@@ -145,10 +106,50 @@ public class Main {
 		// String string = BinaryTrees.printString(bsts.get(0));
 		// Files.writeToFile("F://1.txt", string);
 	}
+
+	static final String LINE = "----------------------------------";
+	static List<BinarySearchTree<Integer>> bsts = new ArrayList<>();
 	
-	public static void main(String[] args) {
-		tree1();
-		tree2();
-		tree3();
+	static BinarySearchTree<Integer> bst(Integer ints[]) {
+		if (ints == null) return null;
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		for (Integer integer : ints) {
+			bst.add(integer);
+		}
+		return bst;
+	}
+	
+	static {
+		bsts.add(bst(new Integer[]{
+				7,4,9,2,5,8,11,1,3,6,10,12
+			}));
+		bsts.add(bst(new Integer[]{
+				381, 12, 410, 9, 40, 394, 540, 
+				35, 190, 476, 760, 146, 445,
+				600, 800
+			}));
+		
+		// 二叉树数量
+		int bstCount = 3;
+		for (int i = 0; i < bstCount; i++) {
+			BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+			bsts.add(bst);
+			
+			// 二叉树的节点数量
+			int nodeCount = (int) (10 + Math.random() * 30);
+			for (int j = 0; j < nodeCount; j++) {
+				double random = Math.random();
+				// 每个节点的值
+				int element = 0;
+				if (random > 0.6) {
+					element = (int) (Math.random() * 10000);
+				} else if (random > 0.3) {
+					element = (int) (Math.random() * 1000);
+				} else {
+					element = (int) (Math.random() * 100);
+				}
+				bst.add(element);
+			}
+		}
 	}
 }
